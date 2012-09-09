aa9dccaca4bab511ad644fb6b3e302952cc3bf5b
orankaneh <rizaldymulyatno@gmail.com>
2012-09-10 06:58:19 +0700

    update

--- app/about.php
+++ app/about.php
@@ -6,7 +6,7 @@
 					<p>Free Smsgateway Pengisian Pulsa By jogjaweb.in adalah program gratis yang diberikan jogjaweb.in untuk membantu proses pengisian pulsa di counter hp.diharapkan dengan program ini dapat membantu proses pengisin pulsa di hp tanpa harus mengetikan perintah sms yang panjang.Free Smsgateway Pengisian Pulsa By jogjaweb.in mempunya fitur sebagai berikut:</p>
 
 					<ul>
-						<li>Gammu web gui installer </li>
+						<li>Gammu web gui installer[by rosihanari http://blog.rosihanari.net] </li>
 						<li>Database web gui config</li>
 						<li>Kirim sms dari template yang sudah ditentukan</li>
 					</ul>
